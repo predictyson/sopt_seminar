38d1656a585a7732aae17ff6793ff5fb4964de89
yezgoget <55784772+yezgoget@users.noreply.github.com>
2019-12-19 02:30:51 +0900

    assginment2

---
+++ module/groupMixer.js
@@ -0,0 +1,15 @@
+const groupMixer = {
+    mix: (memberArray)=>{
+        const mixArray = [...memberArray];
+        mixArray.forEach(element =>{
+
+            const targetIdx = parseInt(Math.random()*(memberArray.length-1));
+            const tmp = element.groupIdx;
+            element.groupIdx = mixArray[targetIdx].groupIdx;
+            mixArray[targetIdx].groupIdx = tmp;
+        });
+        return mixArray;
+
+    }
+};
+module.exports = groupMixer;
---
+++ routes/api/group.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const csvManager = require('../../module/csvManager');
+const groupMixer = require('../../module/groupMixer');
+
+router.get('/', async(req, res)=>{
+    //try-catch 상위 레퍼 메소드에서 에러 핸들링.
+    try{
+        const groupArray = await csvManager.read('group.csv');
+        console.log(groupArray);
+        const groupMap ={};
+        groupArray.forEach(element =>{
+            groupMap[element.groupIdx]=element.name;
+        });
+        const memberArray = await csvManager.read('member.csv');
+        res.status(200).send(memberArray.map(it => `${it.name}:${groupMap[it.groupIdx]}`).join('\n'));
+    }catch(err){
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+    }
+    });
+
+    router.get('/mixer', async(req, res)=>{
+        try{
+            const memberArray = await csvManager.read('member.csv');
+            const newArray = groupMixer.mix(memberArray);
+            await csvManager.write('member.csv', newArray);
+            res.status(200).send('success to mix group');
+        }catch(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        });
+
+        router.get('/:groupIdx', async (req, res) => {
+            const groupIdx = req.params.groupIdx;
+            try {
+                const groupArray = await csvManager.read('group.csv');
+                console.log(groupArray);
+                const groupMap = {};
+                groupArray.forEach(element => {
+                    groupMap[element.groupIdx] = element.name;
+                });
+                const memberArray = await csvManager.read('member.csv');
+                res.status(200).send(memberArray
+                    .filter(it => it.groupIdx == groupIdx)
+                    .map(it => `${it.name}:${groupMap[it.groupIdx]}`)
+                    .join('\n'));
+            } catch (err) {
+                console.log(err);
+                res.status(500).send('Internal Server Error');
+            }
+        });
+        
+        module.exports = router;
